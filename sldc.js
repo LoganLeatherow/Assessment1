8e1e9d10d4a02e5a6c8e90127376cb23abcdb291
Logan Leatherow <lleatherow@niagarawater.com>
2022-04-16 17:48:35 -0500

    completed

--- sldc.js
+++ sldc.js
@@ -6,16 +6,20 @@
 */
 
 // Analysis of Requirements
+// Planning
 // Design
+// Testing & Integration
 // Implementation
 // Maintain
-// Planning
-// Testing & Integration
-
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+let analysisOfRequirements = "Identifying what the current problems are. Meeting with customers and stakeholders to define the ask."
+let planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+let design = "Creating a story of what will be done. Creating code from that structure"
+let testingAndIntegration = "Test for defects and deficiencies. Verification of proper use through QA."
+let implementation = "Put the work to use"
+let maintain = "Continued progress and improved efficiency."
